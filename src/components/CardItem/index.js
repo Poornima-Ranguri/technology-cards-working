f18c31e046483609b3f5dc4f9a13493b30fc3f30
Poornima-Ranguri <poornimaranguri@gmail.com>
2023-12-14 17:10:32 +0000

    Technology Cards Output sample

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,14 @@
+// Write your code here.
+
+const CardCreating = () => {
+  return (
+    <li className="user-card-container card-1">
+      <div className="text-container">
+        <h1 className="head">heading</h1>
+        <p className="para">paragraph</p>
+      </div>
+    </li>
+  )
+}
+
+export default CardCreating
